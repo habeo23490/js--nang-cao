471c877af1b6e7e3bfd552599e0e712833e52408
Ha <ha12bnbk@gmail.com>
2022-08-28 21:42:41 +0700

    js

--- b2/homework/java.js
+++ b2/homework/java.js
@@ -1,3 +1,15 @@
+// Yêu cầu
+// Giá trị mặc định của counter ban đầu = 0
+
+// Bấm vào Cộng tăng counter lên 1
+
+// Bấm vào Trừ giảm counter đi 1
+
+// Nếu counter > 0 có màu green
+// Nếu counter = 0 có màu #333333
+// Nếu counter < 0 có màu red
+
+
 const count1 = document.querySelector(".btn-container");
 let x = 0 ; 
 let count2 = count1.querySelector(".prevBtn")
